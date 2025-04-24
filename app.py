fe72b8eded231ca2a2a545dad441612253a791f6
rong171105 <171105@yahoo.com.tw>
2025-04-24 11:08:41 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask, request
+
 app = Flask(__name__)
 
 @app.route("/callback", methods=["POST"])
 def callback():
+    print("收到來自 LINE 的訊息：", request.json)
     return "OK", 200
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=10000)
